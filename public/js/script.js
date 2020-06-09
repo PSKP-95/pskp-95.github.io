5a310c4affe024d2fb974de17d56e8381b6522cd
PSKP-95 <patil8698parikshit@gmail.com>
2020-06-09 15:49:30 +0530

    horizontal status

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,12 @@
     checkbox.checked = false;
   }, false);
 })(document);
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
